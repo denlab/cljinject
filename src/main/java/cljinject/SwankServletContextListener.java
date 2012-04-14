23cbf0ee90ab4c54268b4e533e3a9562264d8965
Denis Labaye <denis.labaye@gmail.com>
2012-04-14 18:50:05 +0200

    launching a thread so that the context listner dont blocks the startup of the app

--- src/main/java/cljinject/SwankServletContextListener.java
+++ src/main/java/cljinject/SwankServletContextListener.java
@@ -11,6 +11,21 @@ public class SwankServletContextListener implements ServletContextListener{
         context = contextEvent.getServletContext();
         // set variable to servlet context
         context.setAttribute("TEST", "TEST_VALUE");
+        
+        System.out.println(">>>>>>>>>>>>>>> now waiting ... ;)");
+
+        new Thread() {
+            public void run() {
+                try {
+                    while (true) {
+                        System.out.print(".");
+                        Thread.sleep(100);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }.start();
     }
     public void contextDestroyed(ServletContextEvent contextEvent) {
         context = contextEvent.getServletContext();
